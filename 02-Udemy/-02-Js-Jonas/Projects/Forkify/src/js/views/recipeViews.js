08a3a7b34c56fd7d83200df7aa71c8a4c5037a34
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-08-28 00:19:41 +0200

    Fixing recipe image

--- 02-Udemy/-02-Js-Jonas/Projects/Forkify/src/js/views/recipeViews.js
+++ 02-Udemy/-02-Js-Jonas/Projects/Forkify/src/js/views/recipeViews.js
@@ -33,7 +33,9 @@ class Recipeview extends View {
   _generateMarkup() {
     return `
       <figure class="recipe__fig">
-        <img src="${this._data.image}" alt="${this._data.title}" />
+        <img src="${this._data.image}" alt="${
+      this._data.title
+    }" class="recipe__img" />
         <h1 class="recipe__title">
           <span>${this._data.title}</span>
         </h1>
