bcad185224aafa9494f23cc9eb1019f09a2b118c
Ahmed Eid <Ahmed_eid67@outlook.com>
2022-08-28 06:40:37 +0200

    Quotes app

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/QuoteDetail.js
@@ -0,0 +1,65 @@
+import { Fragment, useEffect } from "react";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focus">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuote || loadedQuote.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
